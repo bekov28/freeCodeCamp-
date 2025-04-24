7341f6d058774a08f2d9bc9005303f3eded3de3b
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-04-25 07:13:57 +0900

    js work

---
+++ JavaScript Algorithms/DecimalToBinary/script.js
@@ -0,0 +1,33 @@
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
+
+const decimalToBinary = (input) => {
+  let binary = "";
+
+  
+
+  result.innerText = binary;
+};
+
+const checkUserInput = () => {
+  if (
+    !numberInput.value ||
+    isNaN(parseInt(numberInput.value)) ||
+    parseInt(numberInput.value) < 0
+  ) {
+    alert("Please provide a decimal number greater than or equal to 0");
+    return;
+  }
+
+  decimalToBinary(parseInt(numberInput.value));
+  numberInput.value = "";
+};
+
+convertBtn.addEventListener("click", checkUserInput);
+
+numberInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    checkUserInput();
+  }
+});
