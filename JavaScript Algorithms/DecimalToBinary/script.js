99da2636b7718f432f9b1b85945fbcfe3421e053
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-04-27 22:04:14 +0900

    refactored with recursion

--- JavaScript Algorithms/DecimalToBinary/script.js
+++ JavaScript Algorithms/DecimalToBinary/script.js
@@ -3,11 +3,13 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
-  let binary = "";
-
-  
-
-  result.innerText = binary;
+  if (input === 0) {
+    return "0";
+  } else if (input === 1) {
+    return "1";
+  } else {
+    return decimalToBinary(Math.floor(input / 2)) + (input % 2);
+  }
 };
 
 const checkUserInput = () => {
@@ -20,7 +22,7 @@ const checkUserInput = () => {
     return;
   }
 
-  decimalToBinary(parseInt(numberInput.value));
+  result.textContent = decimalToBinary(parseInt(numberInput.value));
   numberInput.value = "";
 };
 
