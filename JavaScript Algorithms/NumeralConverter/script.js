456bc29831d2d950c66c084f8f3d3cbb6d716f50
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-04-29 07:11:21 +0900

    roman number converter project added

---
+++ JavaScript Algorithms/NumeralConverter/script.js
@@ -0,0 +1,18 @@
+const convertBtn = document.getElementById('convert-btn');
+const inputNumber = document.getElementById('number');
+const outputDiv = document.getElementById('output');
+
+convertBtn.addEventListener('click', function() {
+  if(inputNumber.value === '') {
+    outputDiv.textContent = "Please enter a valid number";
+  } else if (inputNumber.value <= 0) {
+    outputDiv.textContent = "Please enter a number greater than or equal to 1";
+  }
+  else if (inputNumber.value >= 4000) {
+    outputDiv.textContent = "Please enter a number less than or equal to 3999";
+  } 
+});
+
+function switchCase() {
+  
+}
