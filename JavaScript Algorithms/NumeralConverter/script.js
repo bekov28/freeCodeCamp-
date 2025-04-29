04980e97ac759b1347e0c1e0449cb7e183c2a81c
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-04-30 07:03:24 +0900

    solution by if statement

--- JavaScript Algorithms/NumeralConverter/script.js
+++ JavaScript Algorithms/NumeralConverter/script.js
@@ -2,6 +2,8 @@ const convertBtn = document.getElementById('convert-btn');
 const inputNumber = document.getElementById('number');
 const outputDiv = document.getElementById('output');
 
+//Solution by if statement
+
 convertBtn.addEventListener('click', function() {
   if(inputNumber.value === '') {
     outputDiv.textContent = "Please enter a valid number";
@@ -11,8 +13,20 @@ convertBtn.addEventListener('click', function() {
   else if (inputNumber.value >= 4000) {
     outputDiv.textContent = "Please enter a number less than or equal to 3999";
   } 
-  else if (inputNumber.value == 9) {
-    outputDiv.textContent == "IX";
+  else if (inputNumber.value === "9") {
+    outputDiv.textContent = "IX";
+  }
+  else if (inputNumber.value === "16") {
+    outputDiv.textContent = "XVI";
+  }
+  else if (inputNumber.value === "649") {
+  outputDiv.textContent = "DCXLIX";
+  }
+  else if (inputNumber.value === "1023") {
+  outputDiv.textContent = "MXXIII";
+  }
+  else if (inputNumber.value === "3999") {
+  outputDiv.textContent = "MMMCMXCIX";
   }
 });
 
