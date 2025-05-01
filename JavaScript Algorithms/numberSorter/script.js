2d8588fa09dc2fc6e8d21122cea30d23d9bc544c
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-05-02 07:17:35 +0900

    sorting method: bubble sort added

--- JavaScript Algorithms/numberSorter/script.js
+++ JavaScript Algorithms/numberSorter/script.js
@@ -3,8 +3,24 @@ const sortButton = document.getElementById("sort");
 const sortInputArray = (event) => {
   event.preventDefault();
 
-  const inputValues = document.getElementsByClassName("values-dropdown");
-  
+  const inputValues = [
+    ...document.getElementsByClassName("values-dropdown")
+  ].map((dropdown) => Number(dropdown.value));
+
+  updateUI(inputValues);
+}
+
+const updateUI = (array = []) => {
+  array.forEach((num, i) => {
+    const outputValueNode = document.getElementById(`output-value-${i}`);
+    outputValueNode.innerText = num;
+  })
+}
+
+const bubbleSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+
+  }
 }
 
 sortButton.addEventListener("click", sortInputArray);
