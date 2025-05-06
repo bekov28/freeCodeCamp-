c29ed26d5e153e15a3f1db43a3baa1fdb2e3242d
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-05-06 16:57:49 +0900

    getMode function updated

--- JavaScript Algorithms/statisticsCalculator/script.js
+++ JavaScript Algorithms/statisticsCalculator/script.js
@@ -1,19 +1,32 @@
 const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
 
 const getMedian = (array) => {
-  const sorted = array.sort((a, b) => a - b);
+  const sorted = array.toSorted((a, b) => a - b);
   const median =
-    array.length % 2 === 0
-      ? getMean([sorted[array.length / 2], sorted[array.length / 2 - 1]])
-      : sorted[Math.floor(array.length / 2)];
+    sorted.length % 2 === 0
+      ? getMean([sorted[sorted.length / 2], sorted[sorted.length / 2 - 1]])
+      : sorted[Math.floor(sorted.length / 2)];
   return median;
 }
 
 const getMode = (array) => {
   const counts = {};
-  array.forEach((el) => counts[el] = counts[el] ? counts[el] + 1 : 1);
-  return counts;
+  array.forEach((el) => {
+    counts[el] = (counts[el] || 0) + 1;
+  })
+  if (new Set(Object.values(counts)).size === 1) {
+    return null;
+  }
+  const highest = Object.keys(counts).sort(
+    (a, b) => counts[b] - counts[a]
+  )[0];
+  const mode = Object.keys(counts).filter(
+    (el) => counts[el] === counts[highest]
+  );
+  return mode.join(", ");
 }
+
+
 
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
@@ -22,8 +35,9 @@ const calculate = () => {
   
   const mean = getMean(numbers);
   const median = getMedian(numbers);
-  console.log(getMode(numbers));
+  const mode = getMode(numbers);
 
   document.querySelector("#mean").textContent = mean;
   document.querySelector("#median").textContent = median;
+  document.querySelector("#mode").textContent = mode;
 }
