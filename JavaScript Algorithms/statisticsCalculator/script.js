0c8dc2badcc2b56a6431bc65626bf8fbc8424045
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-05-06 11:49:13 +0900

    mode added

--- JavaScript Algorithms/statisticsCalculator/script.js
+++ JavaScript Algorithms/statisticsCalculator/script.js
@@ -1,17 +1,19 @@
-
 const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
 
 const getMedian = (array) => {
-  const sorted = array.toSorted((a, b) => a - b);
+  const sorted = array.sort((a, b) => a - b);
   const median =
-    sorted.length % 2 === 0
-      ? getMean([sorted[sorted.length / 2], sorted[sorted.length / 2 - 1]])
-      : sorted[Math.floor(sorted.length / 2)];
+    array.length % 2 === 0
+      ? getMean([sorted[array.length / 2], sorted[array.length / 2 - 1]])
+      : sorted[Math.floor(array.length / 2)];
   return median;
 }
 
-
-
+const getMode = (array) => {
+  const counts = {};
+  array.forEach((el) => counts[el] = counts[el] ? counts[el] + 1 : 1);
+  return counts;
+}
 
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
@@ -20,6 +22,7 @@ const calculate = () => {
   
   const mean = getMean(numbers);
   const median = getMedian(numbers);
+  console.log(getMode(numbers));
 
   document.querySelector("#mean").textContent = mean;
   document.querySelector("#median").textContent = median;
