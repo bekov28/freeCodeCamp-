14d6ccd4bf83deb4ceb92b11b68a8c8b3bc1ffd8
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-05-12 07:06:33 +0900

    practice

--- Practice/script.js
+++ Practice/script.js
@@ -5,3 +5,11 @@
 //   count += 1;
 // }
 // console.log(count); //55
+
+const arrayList = [1, 8, 4, 3, 4, 2, 4, 5];
+const sortedArray = arrayList.sort(); //[1, 2, 3, 4, 4, 4, 5 ,8]
+console.log(sortedArray);
+
+const duplicateRemove = sortedArray.filter((num, index) => sortedArray.indexOf(num) === index);
+console.log(duplicateRemove);
+
