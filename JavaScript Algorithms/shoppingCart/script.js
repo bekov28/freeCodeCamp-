1d487ff417249bf8cbf2ee7f77f0693fcd498e9a
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-05-16 23:28:44 +0900

    shopping card function updated

--- JavaScript Algorithms/shoppingCart/script.js
+++ JavaScript Algorithms/shoppingCart/script.js
@@ -134,3 +134,18 @@ class ShoppingCart {
   }
 };
 
+const cart = new ShoppingCart();
+const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
+
+[...addToCartBtns].forEach(
+  (btn) => {
+    btn.addEventListener("click", (event) => {
+      cart.addItem(Number(event.target.id), products);
+    })
+  }
+);
+
+cartBtn.addEventListener("click", () => {
+  isCartShowing = !isCartShowing;
+
+});
