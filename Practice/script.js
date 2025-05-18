0ea1fc946f303a12b57c91fa2c9a218d16841f11
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-05-18 18:03:01 +0900

    array practice

--- Practice/script.js
+++ Practice/script.js
@@ -13,28 +13,89 @@
 // const duplicateRemove = sortedArray.filter((num, index) => sortedArray.indexOf(num) === index);
 // console.log(duplicateRemove);
 
-$('li input[type="checkbox"]').change(function() {
-    const $allItems = $('li');
-    const $currentItem = $(this).closest('li');
-    const currentIndex = $allItems.index($currentItem);
+
+
+// $('li input[type="checkbox"]').change(function() {
+//     const $allItems = $('li');
+//     const $currentItem = $(this).closest('li');
+//     const currentIndex = $allItems.index($currentItem);
     
-    // Find first unchecked item
-    const $firstUnchecked = $allItems.filter(function() {
-      return !$(this).find('input[type="checkbox"]').is(':checked');
-    }).first();
+//     // Find first unchecked item
+//     const $firstUnchecked = $allItems.filter(function() {
+//       return !$(this).find('input[type="checkbox"]').is(':checked');
+//     }).first();
   
-    // If current item is checked and there are earlier unchecked items
-    if ($(this).is(':checked') && $allItems.index($firstUnchecked) < currentIndex) {
-      // Scroll to first unchecked item
-      $firstUnchecked[0].scrollIntoView({ behavior: 'smooth' });
+//     // If current item is checked and there are earlier unchecked items
+//     if ($(this).is(':checked') && $allItems.index($firstUnchecked) < currentIndex) {
+//       // Scroll to first unchecked item
+//       $firstUnchecked[0].scrollIntoView({ behavior: 'smooth' });
       
-      // Optional: Highlight it
-      $firstUnchecked.css('background', '#fff3cd').siblings().css('background', '');
+//       // Optional: Highlight it
+//       $firstUnchecked.css('background', '#fff3cd').siblings().css('background', '');
       
-      // Optional: Focus the checkbox
-      $firstUnchecked.find('input').focus();
+//       // Optional: Focus the checkbox
+//       $firstUnchecked.find('input').focus();
       
-      // Prevent checking (optional)
-      // $(this).prop('checked', false);
-    }
-  });
+//       // Prevent checking (optional)
+//       // $(this).prop('checked', false);
+//     }
+//   });
+
+//Cloning and spread
+
+//Yuzaki klonlash
+//Chuqur klonlash
+
+const person = {
+  name: 'Berd',
+  age: 33,
+  hobbies: {
+    games: 'chess',
+    sport: 'boxing'
+  }
+}
+//const secondPerson = person;
+//console.log(secondPerson);
+// secondPerson.name = 'Samar'
+// secondPerson.age = 23
+
+// console.log(person);  //{name: 'Samar', age: 23}
+// console.log(secondPerson);  //{name: 'Samar', age: 23}
+
+//#1. Iteratsiya yordamida klonlash:
+
+// const copyObj = obj => {
+//   const clone = {};
+
+//   for(let key in obj) {
+//     clone[key] = obj[key]
+//   }
+//   return clone;
+// }
+// const secondPerson = copyObj(person);
+// secondPerson.hobbies.games = 'lanka';
+// console.log(secondPerson)
+
+//#2 Object assign
+
+// const secondPerson = Object.assign({isMarried: false}, person);
+
+// secondPerson.name = 'Samar',
+// secondPerson.age = 23
+
+// console.log(person); 
+// console.log(secondPerson);  
+
+//#3 Spread operatori:
+
+// const secondPerson = {...person};
+// console.log(secondPerson);
+
+function spread(a, b, c) {
+  console.log('First arg: ', a)
+  console.log('Second arg: ', b)
+  console.log('Third arg: ', c)
+}
+
+const arr = [1, 2, 3];
+spread(...arr);
